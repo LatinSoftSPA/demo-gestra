f2099b323150df57f31d848c3a84dcc94dba037a
LatinSoftSPA <latinsoft.chile@gmail.com>
2021-11-01 23:08:22 -0300

    UpDate 10.1.1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('css/normalize.css') }}" defer></script>
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="fondo">
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
+</html>
